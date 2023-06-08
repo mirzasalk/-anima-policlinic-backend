3872e906b2288650f3fdc4b056545811e0b5fe4a
mirza salkovic <salkovic.mirza@gmail.com>
2023-06-08 20:12:15 +0200

    add backend setup

---
+++ config/dbConfig.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+}
+const db = mongoose.connection;
+
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+module.exports = mongoose;
