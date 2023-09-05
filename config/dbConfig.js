0a4b9588f6430003c9da72851e9e20f2d6193eea
mirza salkovic <salkovic.mirza@gmail.com>
2023-09-05 20:31:52 +0200

    samir

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,20 +1,6 @@
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-
-// main().catch((err) => console.log(err));
-
-// async function main() {
-//   await mongoose.connect(process.env.MONGO_URL);
-// }
-// const db = mongoose.connection;
-
-// db.once("open", function () {
-//   console.log("Connected successfully");
-// });
-// module.exports = mongoose;
 const mongoose = require("mongoose");
 require("dotenv").config();
-//OVDE JE SVE VEZANO ZA KONEKCIJU SA BAZOM
+
 const connectDatabase = () => {
   mongoose.connect(process.env.MONGO_URL);
 
